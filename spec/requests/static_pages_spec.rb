ddec6e5c83ad9c5e4e146b145b934811048f4f93
William Stackhouse <wstackhouse@comcast.net>
2014-02-11 21:17:27 -0500

    Finish static pages

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,43 @@
+require 'spec_helper'
+
+describe "Static pages" do
+
+  describe "Home page" do
+
+    it "should have the content 'A Friend Abroad'" do
+      visit '/static_pages/home'
+      expect(page).to have_content('A Friend Abroad')
+    end
+	
+	 it "should have the title 'Home'" do
+      visit '/static_pages/home'
+      expect(page).to have_title("A Friend Abroad | Home")
+    end
+  end
+  
+  describe "Help page" do
+
+    it "should have the content 'Help'" do
+      visit '/static_pages/help'
+      expect(page).to have_content('Help')
+    end
+	
+	 it "should have the title 'Help'" do
+      visit '/static_pages/help'
+      expect(page).to have_title("A Friend Abroad | Help")
+    end
+  end
+  
+  describe "About page" do
+
+    it "should have the content 'About Us'" do
+      visit '/static_pages/about'
+      expect(page).to have_content('About Us')
+    end
+	
+	 it "should have the title 'About Us'" do
+      visit '/static_pages/about'
+      expect(page).to have_title("A Friend Abroad | About Us")
+    end
+  end
+end
