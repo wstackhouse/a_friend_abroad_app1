fd5014ccd5ccd310ef6d52c4deb7a03c150c355d
William Stackhouse <wstackhouse@comcast.net>
2014-02-13 20:52:03 -0500

    Finish layout and routes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,42 +2,34 @@ require 'spec_helper'
 
 describe "Static pages" do
 
+  subject { page }
+
   describe "Home page" do
+    before { visit root_path }
 
-    it "should have the content 'A Friend Abroad'" do
-      visit '/static_pages/home'
-      expect(page).to have_content('A Friend Abroad')
-    end
-	
-	 it "should have the title 'Home'" do
-      visit '/static_pages/home'
-      expect(page).to have_title("A Friend Abroad | Home")
-    end
+    it { should have_content('A Friend Abroad') }
+    it { should have_title(full_title('')) }
+    it { should_not have_title('| Home') }
   end
-  
+
   describe "Help page" do
+    before { visit help_path }
 
-    it "should have the content 'Help'" do
-      visit '/static_pages/help'
-      expect(page).to have_content('Help')
-    end
-	
-	 it "should have the title 'Help'" do
-      visit '/static_pages/help'
-      expect(page).to have_title("A Friend Abroad | Help")
-    end
+    it { should have_content('Help') }
+    it { should have_title(full_title('Help')) }
   end
-  
-  describe "About page" do
 
-    it "should have the content 'About Us'" do
-      visit '/static_pages/about'
-      expect(page).to have_content('About Us')
-    end
-	
-	 it "should have the title 'About Us'" do
-      visit '/static_pages/about'
-      expect(page).to have_title("A Friend Abroad | About Us")
-    end
+  describe "About page" do
+    before { visit about_path }
+
+    it { should have_content('About') }
+    it { should have_title(full_title('About Us')) }
+  end
+
+  describe "Contact page" do
+    before { visit contact_path }
+
+    it { should have_content('Contact') }
+    it { should have_title(full_title('Contact')) }
   end
 end
